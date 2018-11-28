95534cd434912ce3ca8012ecaec38dbe5bcd89a0
Alon Bibring <alon.bibring@gmail.com>
2018-11-28 12:23:47 -0500

    .gitignore fix

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  GITHUB_CLIENT_ID: 'fd744291ef509a6a54cc',
-  GITHUB_CLIENT_SECRET: 'ad8ff2c72a4ffcbfc9d2c98e441fb4d192eba8be',
-  GITHUB_CALLBACK_URL: 'http://localhost:3000/user/signin/callback',
-  MONGO_URL: 'mongodb://alon:alon11@ds237932.mlab.com:37932/fullstack-review',
-  CRYPTR_SECRET: 'inefu9h4820qj09q3m19dm09m'
-};
