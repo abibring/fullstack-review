f3b1a9b993ebf7da6a51211b3eda2ea86143f94b
Alon Bibring <alon.bibring@gmail.com>
2018-12-11 00:45:45 -0500

    s

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const router = require('./server/routes/index.js');
 const cookieSession = require('cookie-session');
 const path = require('path');
 
 const app = express();
 
-app.use(cookieSession({
-  name: 'session',
-  secret: 'splurgesauce',
-  saveUninitialized: true,
-  maxAge: 24 * 60 * 60 * 1000 // 24 hours
-}));
-console.log('X', path.join(__dirname, '/client/dist'))
+app.use(
+  cookieSession({
+    name: 'session',
+    secret: 'splurgesauce',
+    saveUninitialized: true,
+    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+  })
+);
 app.use(express.static(path.join(__dirname, '/client/dist'), { redirect: false }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/', router);
 app.use('/*', (req, res) => {
-  res.redirect('/')
-})
+  res.redirect('/');
+});
 
 let port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`listening on port ${port}`));
-
