af2ad49a733b8e0c35b0adb4cacba6e8ae1f4e0e
Alon Bibring <alon.bibring@gmail.com>
2018-12-12 14:55:14 -0500

    editing signout button

--- client/src/components/NavigationBar.jsx
+++ client/src/components/NavigationBar.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Navbar, NavItem, Nav, Image } from 'react-bootstrap';
+import { Navbar, NavItem, Nav, Button } from 'react-bootstrap';
 import logo from '../../dist/img/logo.png';
 
 export default class NavigationBar extends Component {
@@ -9,7 +9,7 @@ export default class NavigationBar extends Component {
   render() {
     const { signOut } = this.props;
     return (
-      <Navbar inverse bsStyle="pills">
+      <Navbar inverse>
         <Navbar.Header>
           <Navbar.Brand>
             <img src={logo} style={{ width: 260, height: 80 }} alt="logo"/>
@@ -17,7 +17,7 @@ export default class NavigationBar extends Component {
         </Navbar.Header>
         <Nav pullRight>
           <NavItem eventKey={1} href="#" onClick={signOut} >
-            Sign Out
+            <Button>Sign Out</Button>
           </NavItem>
         </Nav>
       </Navbar>
