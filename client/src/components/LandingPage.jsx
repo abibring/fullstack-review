693c472ec2091164aa1bdc1ec5a1ab6102be45c1
Alon Bibring <alon.bibring@gmail.com>
2018-12-12 14:46:34 -0500

    styling landing page photo

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -9,17 +9,17 @@ export default class LandingPage extends Component {
   }
   render() {
     return (
-      <Jumbotron style={{ backgroundImage: "url('https://i.pinimg.com/originals/b7/46/43/b74643cbbe7d68c54554544ec50cf999.jpg')", height: 1080, width: 1920 }}>
+      <Jumbotron style={{ backgroundImage: "url('https://i.pinimg.com/originals/b7/46/43/b74643cbbe7d68c54554544ec50cf999.jpg')", height: 864, width: 1344 }}>
         <h1 style={{ paddingLeft: 12 }}>The Better Github News Feed</h1>
         <p style={{ paddingLeft: 15, paddingRight: 15, paddingTop: 5, fontSize: 25 }}>
           This is a better version of the Github feed, which allows you to track the latest information
           about repos you are watching, contributing to, or using.  Get the latest information about any updates,
           such as pull requests, commits, issues, new repos, and more! Sign in with your Github credentials to get started!
         </p> <br />
-        <p style={{ paddingLeft: 10 }}>
+        <div style={{ paddingLeft: 10 }}>
           <Login />
           {/* <Signup /> */}
-        </p>
+        </div>
       </Jumbotron>
     );
   }
