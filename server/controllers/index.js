1533486975257d5a8b92d6276c4235ab476a0f26
Alon Bibring <alon.bibring@gmail.com>
2018-12-12 13:17:59 -0500

    changed localStorage setting

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -51,7 +51,7 @@ module.exports = {
                             <body>
                               <script>
                                   window.localStorage.setItem('userToken', '${encryptedToken}');
-                                  window.localStorage.setItem('username', '${JSON.stringify(resultsTwo)}');
+                                  window.localStorage.setItem('username', '${JSON.stringify(data)}');
                                   window.location.pathname = '/home';
                               </script>
                             </body>
@@ -64,7 +64,7 @@ module.exports = {
                         <body>
                           <script>
                               window.localStorage.setItem('userToken', '${encryptedToken}');
-                              window.localStorage.setItem('username', '${JSON.stringify(resultsOne)}');
+                              window.localStorage.setItem('username', '${JSON.stringify(data)}');
                               window.location.pathname = '/home';
                           </script>
                         </body>
