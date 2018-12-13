7459b93d1c5f2ff7c7878a07bfae045b33cc64dd
Alon Bibring <alon.bibring@gmail.com>
2018-12-13 18:56:22 -0500

    fixing bugs

--- client/src/components/home/HomeFeed.jsx
+++ client/src/components/home/HomeFeed.jsx
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
-import { Tab, Tabs } from 'react-bootstrap';
-import Watching from '../tabComponents/Watching.jsx';
-import Following from '../tabComponents/Following.jsx';
-import Starred from '../tabComponents/Starred.jsx';
-import Releases from '../events/Releases.jsx';
 import Events from '../events/Events.jsx';
+// import Watching from '../tabComponents/Watching.jsx';
+// import Following from '../tabComponents/Following.jsx';
+// import Starred from '../tabComponents/Starred.jsx';
 
 
 export default class HomeFeed extends Component {
   constructor(props, context) {
     super(props, context);
-    this.state = { key: 1 };
   };
 
   render() {
-    const { key } = this.state;
     const { issues, watching, starred, events, notifications } = this.props;
     return (
       <div>
