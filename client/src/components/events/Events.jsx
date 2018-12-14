52535284f246bfd4abac6a35e3af1f28b0aeffb7
Alon Bibring <alon.bibring@gmail.com>
2018-12-13 20:31:42 -0500

    working on sorting by order

--- client/src/components/events/Events.jsx
+++ client/src/components/events/Events.jsx
@@ -10,37 +10,42 @@ import ReleaseEvent from './ReleaseEvent.jsx';
 // import Notification from './Notification.jsx';
 // import AssociatedEvent from './AssociatedEvent.jsx';
 
-const Events = ({ events, notifications, pulls, associated, watching, starred }) => (
-  <div>
-    <h3 style={{ paddingLeft: '40%' }}>Check Out The Latest Info:</h3>
-    {console.log('starred', starred)}
-    <Grid>
-      <Row>
-        <Col xs={12} md={8} xsOffset={2}>
-          <ListGroup>
-            {starred.length > 0 && starred.map(repoArr => (
-                repoArr.map(repo => (
-                  repo.pull_request && !repo.published_at && !repo.state 
-                  ? 
-                  <PullRequest pull={repo} key={repo.id} />
-                  :
-                  repo.state
-                  ?
-                  <IssuesEvent event={repo} key={repo.id} />
-                  :
-                  repo.published_at 
-                  ?
-                  <ReleaseEvent release={repo} key={repo.id} />
-                  :
-                  ''
-                ))
-              ) 
-            )}
-          </ListGroup>
-        </Col>
-      </Row>
-    </Grid>
-  </div>
-);
+const Events = ({ events, notifications, pulls, associated, watching, starred }) => {
+  console.log('UNSORTED', starred.flat())
+  let starredSorted = starred.flat().sort((a,b) => new Date(b.updated_at || b.created_at) - new Date(a.updated_at || a.created_at));
+  console.log('SORTED', starredSorted)
+  return (
+    <div>
+      <h3 style={{ paddingLeft: '40%' }}>Check Out The Latest Info:</h3>
+      {console.log('starred', starred)}
+      <Grid>
+        <Row>
+          <Col xs={12} md={8} xsOffset={2}>
+            <ListGroup>
+              {starred.length > 0 && starred.map(repoArr => (
+                  repoArr.map(repo => (
+                    repo.pull_request && !repo.published_at && !repo.state 
+                    ? 
+                    <PullRequest pull={repo} key={repo.id} />
+                    :
+                    repo.state
+                    ?
+                    <IssuesEvent event={repo} key={repo.id} />
+                    :
+                    repo.published_at 
+                    ?
+                    <ReleaseEvent release={repo} key={repo.id} />
+                    :
+                    ''
+                  ))
+                ) 
+              )}
+            </ListGroup>
+          </Col>
+        </Row>
+      </Grid>
+    </div>
+  );
+};
 
 export default Events;
