b2e1f272906f192279d5f0bbd583541202f9504a
Alon Bibring <alon.bibring@gmail.com>
2018-12-13 19:44:42 -0500

    changed border color for events to black

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,18 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Button } from 'react-bootstrap';
 
-export default class Login extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-        <Button
-          href={`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&scope=user%20repo%20notifications`}
-          bsStyle="primary"
-        >
-          Sign In With Github
-        </Button>
-    );
-  }
-}
+const Login = () => (
+  <Button
+    href={`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&scope=user%20repo%20notifications`}
+    bsStyle="primary"
+  >
+    Sign In With Github
+  </Button>
+)
+
+export default Login;
