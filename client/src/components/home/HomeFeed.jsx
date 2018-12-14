e8cc619c88deecde4139d2ab25afcc9dae567440
Alon Bibring <alon.bibring@gmail.com>
2018-12-13 20:57:12 -0500

    adding loading state and changed some components to stateless

--- client/src/components/home/HomeFeed.jsx
+++ client/src/components/home/HomeFeed.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import Events from '../events/Events.jsx';
-// import Watching from '../tabComponents/Watching.jsx';
-// import Following from '../tabComponents/Following.jsx';
-// import Starred from '../tabComponents/Starred.jsx';
 
 
 const HomeFeed = ({ issues, watching, starred, events, notifications, isLoading }) => (
