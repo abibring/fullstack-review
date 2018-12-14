4f52f6b392fcbc3396663bba5e0c6ecf3cc13365
Alon Bibring <alon.bibring@gmail.com>
2018-12-13 19:26:47 -0500

    fixing brackets in controller index.js

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,11 +1,11 @@
 const { getInfo, saveUser, getUser } = require('../models/index.js');
-const { 
-  getIssuesFromGithub, 
-  getWatchingFromGithub, 
-  authenticateUser, 
-  getTokenForUser, 
-  getUserNotifications, 
-  getStarredRepos, 
+const {
+  getIssuesFromGithub,
+  getWatchingFromGithub,
+  authenticateUser,
+  getTokenForUser,
+  getUserNotifications,
+  getStarredRepos,
   getRepoEvents,
   associatedToIssue,
   getFeedForUser,
@@ -19,7 +19,6 @@ const Cryptr = require('cryptr');
 const cryptr = new Cryptr(process.env.CRYPTR_SECRET);
 
 module.exports = {
-
   login: {
     get: function(req, res) {
       const { query } = req;
@@ -40,60 +39,63 @@ module.exports = {
                     <body>
                       <script>
                           window.localStorage.setItem('userToken', '${encryptedToken}');
-                          window.localStorage.setItem('username', '${data.login}');
+                          window.localStorage.setItem('username', '${
+                            data.login
+                          }');
                           window.location.pathname = '/home';
                       </script>
                     </body>
                 </html>
               `);
             })
-            .catch(err => res.send(err))
-      })
-        .catch(err => res.send(err))
-    },
+            .catch(err => res.send(err));
+        })
+        .catch(err => res.send(err));
+    }
+  },
 
-    signup: {
-      post: function(req, res) {
-        const { query } = req;
-        const { code } = query;
-        if (!code) {
-          return res.send({ success: false, message: 'Error: invalid code' });
-        }
-        getTokenForUser(code)
-          .then(({ data }) => {
-            const access_token = data.split('&')[0];
-            const token = access_token.slice(13);
-            const encryptedToken = cryptr.encrypt(token);
-            authenticateUser(access_token)
-              .then(({ data }) => {
-                 saveUser(data, (err, results) => {
-                  if (err) {
-                    res.send(`
-                      <html>
-                          <body>
-                            <script>
-                                window.localStorage.setItem('userToken', 'invalid');
-                                window.localStorage.setItem('username', 'invalid');
-                                window.location.pathname = '/';
-                            </script>
-                          </body>
-                      </html>`);
-                  } else {
-                    res.send(`
-                      <html>
-                          <body>
-                            <script>
-                                window.localStorage.setItem('userToken', '${encryptedToken}');
-                                window.localStorage.setItem('username', '${results[0].username}');
-                                window.location.pathname = '/home';
-                            </script>
-                          </body>
-                      </html>`);
-                    }
-                  });
-              }).catch(err => console.error('err in savingUser', err));
-            }).catch(err => console.error('err in savingUser Outside', err));
-    },
+  // signup: {
+  //   post: function(req, res) {
+  //     const { query } = req;
+  //     const { code } = query;
+  //     if (!code) {
+  //       return res.send({ success: false, message: 'Error: invalid code' });
+  //     }
+  //     getTokenForUser(code)
+  //       .then(({ data }) => {
+  //         const access_token = data.split('&')[0];
+  //         const token = access_token.slice(13);
+  //         const encryptedToken = cryptr.encrypt(token);
+  //         authenticateUser(access_token)
+  //           .then(({ data }) => {
+  //              saveUser(data, (err, results) => {
+  //               if (err) {
+  //                 res.send(`
+  //                   <html>
+  //                       <body>
+  //                         <script>
+  //                             window.localStorage.setItem('userToken', 'invalid');
+  //                             window.localStorage.setItem('username', 'invalid');
+  //                             window.location.pathname = '/';
+  //                         </script>
+  //                       </body>
+  //                   </html>`);
+  //               } else {
+  //                 res.send(`
+  //                   <html>
+  //                       <body>
+  //                         <script>
+  //                             window.localStorage.setItem('userToken', '${encryptedToken}');
+  //                             window.localStorage.setItem('username', '${results[0].username}');
+  //                             window.location.pathname = '/home';
+  //                         </script>
+  //                       </body>
+  //                   </html>`);
+  //                 }
+  //               });
+  //           }).catch(err => console.error('err in savingUser', err));
+  //         }).catch(err => console.error('err in savingUser Outside', err));
+  // },
   logout: {
     get: function(req, res) {
       req.session = null;
@@ -102,7 +104,7 @@ module.exports = {
   },
 
   events: {
-    get: function (req, res) {
+    get: function(req, res) {
       const { userToken, username } = req.query;
       getRepoEvents(userToken, username)
         .then(({ data }) => {
@@ -110,7 +112,7 @@ module.exports = {
           // let headersSplitOnToken = data.headers.link.split('?');
           // console.log('headersSplitOnToken', headersSplitOnToken);
           // res.setHeader('link', data.headers.link)
-          res.send(data)
+          res.send(data);
         })
         .catch(err => res.send(err));
     }
@@ -123,7 +125,7 @@ module.exports = {
         .then(({ data }) => {
           // res.setHeader('link', data.headers.link)
           // console.log('WATCHING', data.data)
-          res.send(data)
+          res.send(data);
         })
         .catch(err => res.send(err));
     }
@@ -138,23 +140,29 @@ module.exports = {
           // iterate over data & push repo name and owner into reposStarred array
           for (var i = 0; i < data.length; i++) {
             let repo = data[i];
-            reposStarred.push({repo: repo.name, owner: repo.owner.login})
+            reposStarred.push({ repo: repo.name, owner: repo.owner.login });
           }
           // iterate over reposStarred & call issue endpoint for each repo. Store result.
-          const issuePromise = Promise.all(reposStarred.map(repo => {
-            return getRepoIssues(repo.owner, repo.repo, userToken)
-          }));
+          const issuePromise = Promise.all(
+            reposStarred.map(repo => {
+              return getRepoIssues(repo.owner, repo.repo, userToken);
+            })
+          );
           // iterate over reposStarred & call notification endpoint for each repo. Store result.
-          const notificationPromise = Promise.all(reposStarred.map(repo => {
-            return getRepoNotifications(repo.owner, repo.repo, userToken)
-          }));
+          const notificationPromise = Promise.all(
+            reposStarred.map(repo => {
+              return getRepoNotifications(repo.owner, repo.repo, userToken);
+            })
+          );
           // iterate over reposStarred & call release endpoint for each repo. Store result.
-          const releasePromise = Promise.all(reposStarred.map(repo => {
-            return getRepoReleases(repo.owner, repo.repo, userToken)
-          }));
+          const releasePromise = Promise.all(
+            reposStarred.map(repo => {
+              return getRepoReleases(repo.owner, repo.repo, userToken);
+            })
+          );
           // call issuePromise to resolve data from API
           issuePromise
-            .then((dataOne) => {
+            .then(dataOne => {
               // call notificationPromise to resolve data from API
               notificationPromise
                 .then(dataTwo => {
@@ -166,8 +174,11 @@ module.exports = {
                       var results = [];
                       // iterate over tempResult and push the "data" key into results
                       for (var i = 0; i < tempResult.length; i++) {
-                        if (tempResult[i].data && tempResult[i].data.length > 0) {
-                          results.push(tempResult[i].data)
+                        if (
+                          tempResult[i].data &&
+                          tempResult[i].data.length > 0
+                        ) {
+                          results.push(tempResult[i].data);
                         }
                       }
                       return results;
@@ -208,7 +219,7 @@ module.exports = {
       getIssuesFromGithub(userToken)
         .then(({ data }) => {
           // res.setHeader('link', data.headers.link)
-          res.send(data)
+          res.send(data);
         })
         .catch(err => res.send(err));
     }
@@ -227,7 +238,7 @@ module.exports = {
         if (err) {
           res.send(err);
         } else {
-          res.setHeader('link', data.headers.link)
+          res.setHeader('link', data.headers.link);
           res.send(data);
         }
       });
@@ -239,48 +250,4 @@ module.exports = {
       res.redirect('/');
     }
   }
-}
-
-
-          //     saveUser(data, err => {
-          //       if (err) {
-          //         getUser(data.username, (err, results) => {
-          //           if (err) {
-          //             res.send(`
-          //               <html>
-          //                   <body>
-          //                     <script>
-          //                         window.localStorage.setItem('userToken', 'invalid');
-          //                         window.localStorage.setItem('username', 'invalid');
-          //                         window.location.pathname = '/';
-          //                     </script>
-          //                   </body>
-          //               </html>`);
-          //           } else {
-          //             res.send(`
-          //             <html>
-          //                 <body>
-          //                   <script>
-          //                       window.localStorage.setItem('userToken', '${encryptedToken}');
-          //                       window.localStorage.setItem('username', '${data.login}');
-          //                       window.location.pathname = '/home';
-          //                   </script>
-          //                 </body>
-          //             </html>`);
-          //           }
-          //         })
-          //       } else {
-          //         res.send(`
-          //         <html>
-          //             <body>
-          //               <script>
-          //                   window.localStorage.setItem('userToken', '${encryptedToken}');
-          //                   window.localStorage.setItem('username', '${data.login}');
-          //                   window.location.pathname = '/home';
-          //               </script>
-          //             </body>
-          //         </html>`);
-          //       }
-          //     })
-          //   }).catch(err => res.send(err));
-          // }).catch(err => res.send(err));
+};
