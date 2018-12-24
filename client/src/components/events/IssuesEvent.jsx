52e210760ff05327ffc02d5af38a5ffac2e97dc7
Alon Bibring <alon.bibring@gmail.com>
2018-12-24 13:22:45 -0500

    adjusting other events to use local images rather than links

--- client/src/components/events/IssuesEvent.jsx
+++ client/src/components/events/IssuesEvent.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import moment from 'moment';
 import markdown from 'remove-markdown';
 import { ListGroupItem, Image } from 'react-bootstrap';
+import linkIcon from '../../../dist/img/link_icon.png';
+import userIcon from '../../../dist/img/user_icon.png';
+import folderIcon from '../../../dist/img/folder_icon.png';
 
 const IssuesEvent = ({ event }) => {
   const repoUrlSplit = event.repository_url.split('/');
@@ -16,10 +19,10 @@ const IssuesEvent = ({ event }) => {
       <span className="content">
         <span className="event-label">Issue Event</span>
         <span className="content-break"> | </span>{'    '}
-        <Image src="https://github.githubassets.com/images/icons/emoji/unicode/1f4c2.png?v8"  alt="folder image" style={{ height: 28, width: 24 }}/>{'    '}
+        <Image src={folderIcon} alt="folder image" style={{ height: 28, width: 24 }}/>{'    '}
         <span className="repo-name">{repoName}</span>
         <span className="content-break"> | </span>{'    '}
-        <Image src="https://github.githubassets.com/images/icons/emoji/unicode/1f4db.png?v8" alt="user image" style={{ height: 20, width: 24 }} />{'     '}
+        <Image src={userIcon} alt="user image" style={{ height: 20, width: 24 }} />{'     '}
         <span className="repo-owner">{repoOwner}</span>
         <span className="content-break"> | </span>{'    '}
         <i className="content-updated">{moment(event.updated_at).startOf('day').fromNow()}</i>
@@ -34,7 +37,7 @@ const IssuesEvent = ({ event }) => {
         <div><b>Info: </b>{markdown(event.body)}</div>
       }
         <div>
-          <Image src="https://github.githubassets.com/images/icons/emoji/unicode/1f517.png?v8" style={{ height: 30, width: 24 }} />{'     '}
+          <Image src={linkIcon} alt="link icon" style={{ height: 30, width: 24 }} />{'     '}
           <a href={event.html_url} className="event-link">Checkout Issue on Github</a></div>
       </span>
     </ListGroupItem>
--- client/src/components/events/PullRequest.jsx
+++ client/src/components/events/PullRequest.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import moment from 'moment';
 import markdown from 'remove-markdown';
 import { ListGroupItem, Image } from 'react-bootstrap';
+import linkIcon from '../../../dist/img/link_icon.png';
+import userIcon from '../../../dist/img/user_icon.png';
+import folderIcon from '../../../dist/img/folder_icon.png';
 
 const PullRequest = ({ pull }) => (
   <ListGroupItem key={pull.id} className="pull-event">
@@ -12,10 +15,10 @@ const PullRequest = ({ pull }) => (
     <span className="content">
       <span className="event-label">Pull Request</span>
       <span className="content-break"> | </span>{'    '}
-      <Image src="https://github.githubassets.com/images/icons/emoji/unicode/1f4c2.png?v8"  alt="folder image" style={{ height: 28, width: 24 }}/>{'    '}
+      <Image src={folderIcon}  alt="folder image" style={{ height: 28, width: 24 }}/>{'    '}
       <span className="repo-name">{pull.html_url.split('/')[3]}</span>
       <span className="content-break"> | </span>{'    '}
-      <Image src="https://github.githubassets.com/images/icons/emoji/unicode/1f4db.png?v8" alt="user image" style={{ height: 20, width: 24 }} />{'     '}
+      <Image src={userIcon} alt="user image" style={{ height: 20, width: 24 }} />{'     '}
       <span className="repo-owner">{pull.html_url.split('/')[4]}</span>
       <span className="content-break"> | </span>{'    '}
       <i className="content-updated">{moment(pull.updated_at).startOf('day').fromNow()}</i>
@@ -28,7 +31,7 @@ const PullRequest = ({ pull }) => (
       <div><b>Info: </b>{markdown(pull.body)}</div>
       }
       <div>
-        <Image src="https://github.githubassets.com/images/icons/emoji/unicode/1f517.png?v8" style={{ height: 30, width: 24 }} />{'     '}
+        <Image src={linkIcon} alt="link icon" style={{ height: 30, width: 24 }} />{'     '}
         <a href={pull.pull_request.html_url} className="event-link">Checkout Pull Request on Github</a>
       </div>
     </span>
