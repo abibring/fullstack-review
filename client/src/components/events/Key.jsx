f43c815b87b01f5b632b850c1a6d54fc337fbc1f
Alon Bibring <alon.bibring@gmail.com>
2018-12-24 12:57:36 -0500

    working on adjusting level of key

--- client/src/components/events/Key.jsx
+++ client/src/components/events/Key.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Key = () => (
   <div className="key">
-    <ul>Release Event: <span class="dot-release"></span></ul> 
-    <ul>Issue Event: <span class="dot-issue"></span></ul>
-    <ul>Pull Request: <span class="dot-pull"></span></ul>
+    <span>Release Event: <span class="dot-release"></span></span> 
+    <span>Issue Event: <span class="dot-issue"></span></span>
+    <span>Pull Request: <span class="dot-pull"></span></span>
   </div>
 );
 
