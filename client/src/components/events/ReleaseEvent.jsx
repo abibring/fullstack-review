da1ccf393174e0deaeaf6c21c6963b4ab875002e
Alon Bibring <alon.bibring@gmail.com>
2018-12-24 13:20:15 -0500

    added icon images to folder and changed release event to use images from folder instead of html_link

--- client/src/components/events/ReleaseEvent.jsx
+++ client/src/components/events/ReleaseEvent.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import moment from 'moment';
 import markdown from 'remove-markdown';
 import { ListGroupItem, Image } from 'react-bootstrap';
+import linkIcon from '../../../dist/img/link_icon.png';
+import userIcon from '../../../dist/img/user_icon.png';
+import folderIcon from '../../../dist/img/folder_icon.png';
 
 const ReleaseEvent = ({ release }) => (
   <ListGroupItem className="release-event">
@@ -12,10 +15,10 @@ const ReleaseEvent = ({ release }) => (
     <span className="content">
       <span className="event-label">Release Notification</span>
       <span className="content-break"> | </span>{'    '}
-      <Image src="https://github.githubassets.com/images/icons/emoji/unicode/1f4c2.png?v8"  alt="folder image" style={{ height: 28, width: 24 }}/>{'    '}
+      <Image src={folderIcon}  alt="folder image" style={{ height: 28, width: 24 }}/>{'    '}
       <span className="repo-name">{release.html_url.split('/')[3]}</span>
       <span className="content-break"> | </span>{'   '}
-      <Image src="https://github.githubassets.com/images/icons/emoji/unicode/1f4db.png?v8" style={{ height: 20, width: 24 }} />{'     '}
+      <Image src={userIcon} alt="user icon" style={{ height: 20, width: 24 }} />{'     '}
       <span className="repo-owner">{release.html_url.split('/')[4]}</span>
       <span className="content-break"> | </span>{'   '}
       <i className="content-updated">{moment(release.published_at).startOf('day').fromNow()}</i>
@@ -28,7 +31,7 @@ const ReleaseEvent = ({ release }) => (
         <div><b>Info: </b>{markdown(release.body)}</div>
       }
       <div>
-        <Image src="https://github.githubassets.com/images/icons/emoji/unicode/1f517.png?v8" style={{ height: 30, width: 24 }} />{'     '}
+        <Image src={linkIcon} alt="link icon" style={{ height: 30, width: 24 }} />{'     '}
         <a href={release.html_url} className="event-link">Checkout Release on Github</a>
       </div>
     </span>
