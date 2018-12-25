f5eef02f3b7412630dd99486cbe7ec396b94d1d9
Alon Bibring <alon.bibring@gmail.com>
2018-12-25 13:08:19 -0500

    moved function higher up to pass to all events and refactored release to functional

--- client/src/components/events/Events.jsx
+++ client/src/components/events/Events.jsx
@@ -4,7 +4,7 @@ import PullRequest from './PullRequest.jsx';
 import IssuesEvent from './IssuesEvent.jsx';
 import ReleaseEvent from './ReleaseEvent.jsx';
 
-const Events = ({ starred }) => (
+const Events = ({ starred, leave }) => (
   <Grid>
     <Row>
       <Col xs={12} md={8} xsOffset={2}>
@@ -12,15 +12,15 @@ const Events = ({ starred }) => (
           {starred.length > 0 && starred.map(repo =>
             repo.pull_request && !repo.published_at
             ? 
-              <PullRequest pull={repo} key={repo.id} />
+              <PullRequest pull={repo} key={repo.id} leave={leave} />
             : 
               repo.state && !repo.published_at 
             ? 
-              <IssuesEvent event={repo} key={repo.id} />
+              <IssuesEvent event={repo} key={repo.id} leave={leave} />
             : 
             repo.published_at 
             ? 
-              <ReleaseEvent release={repo} key={repo.id} />
+              <ReleaseEvent release={repo} key={repo.id} leave={leave} />
             : 
               ''
           )}
