43ef7ca31520c02edd23f055a81f328954703db0
Alon Bibring <alon.bibring@gmail.com>
2018-12-25 12:22:36 -0500

    adjusting pic for all events

--- client/src/components/events/IssuesEvent.jsx
+++ client/src/components/events/IssuesEvent.jsx
@@ -8,29 +8,31 @@ const IssuesEvent = ({ event }) => {
   const repoName = repoUrlSplit[repoUrlSplit.length-2];
   const repoOwner = repoUrlSplit[repoUrlSplit.length-1];
   return (
-    <ListGroupItem key={event.id} className="issue-event">
+    <div className="outer-event">
       <span className="content-img">
         <img src={event.user.avatar_url} alt='avatar' className="feed-img" />
       </span>
-      <span className="content">
-        <span className="content-user">@{event.user.login} created an issue.</span>{'    '}
-        <i className="content-updated">{moment(event.updated_at).startOf('hour').fromNow()}.</i><br />
-        <div className="repo-name">{repoOwner} / {repoName}</div><br />
-        <div>* {event.title} *</div><br />
-        {/* <div><b>Author Association:</b> {event.author_association}</div> */}
-        {/* <div><b>Ranking:</b> {event.ranking}</div> */}
-        {/* {event.org ? <div><b>Organization:</b> {event.org && event.org.login}</div> : '' } */}
-        {markdown(event.body).length > 500
-        ?
-        <div>{markdown(event.body).slice(0, 500) + ' ...'}</div>
-        :
-        <div>{markdown(event.body)}</div>
-      } <br />
-        <div>
-          <a href={event.html_url} className="event-link">View on Github</a>
-        </div>
-      </span>
-    </ListGroupItem>
+      <ListGroupItem key={event.id} className="issue-event">
+        <span className="content">
+          <span className="content-user">@{event.user.login} created an issue.</span>{'    '}
+          <i className="content-updated">{moment(event.updated_at).startOf('hour').fromNow()}.</i><br />
+          <div className="repo-name">{repoOwner} / {repoName}</div><br />
+          <div>* {event.title} *</div><br />
+          {/* <div><b>Author Association:</b> {event.author_association}</div> */}
+          {/* <div><b>Ranking:</b> {event.ranking}</div> */}
+          {/* {event.org ? <div><b>Organization:</b> {event.org && event.org.login}</div> : '' } */}
+          {markdown(event.body).length > 500
+          ?
+          <div>{markdown(event.body).slice(0, 500) + ' ...'}</div>
+          :
+          <div>{markdown(event.body)}</div>
+        } <br />
+          <div>
+            <a href={event.html_url} className="event-link">View on Github</a>
+          </div>
+        </span>
+      </ListGroupItem>
+    </div>
   );
 }
 
--- client/src/components/events/PullRequest.jsx
+++ client/src/components/events/PullRequest.jsx
@@ -4,27 +4,29 @@ import markdown from 'remove-markdown';
 import { ListGroupItem } from 'react-bootstrap';
 
 const PullRequest = ({ pull }) => (
-  <ListGroupItem key={pull.id} className="pull-event">
+  <div className="outer-event">
     <span className="content-img">
       <img src={pull.user.avatar_url} alt='avatar of pull creator' className="feed-img" />
     </span>
-    <span className="content">
-      <span className="content-user">@{pull.user.login} opened a pull request.</span>{'   '}
-      <i className="content-updated">{moment(pull.updated_at).startOf('hour').fromNow()}.</i><br />
-      <div className="repo-name">{pull.html_url.split('/')[4]} / {pull.html_url.split('/')[3]}</div><br />
-      <div>*  {pull.title}  *</div><br />
-      {/* <div><b>Ranking:</b> {pull.ranking}</div> */}
-      {markdown(pull.body).length > 500
-      ?
-      <div>{markdown(pull.body).slice(0, 500) + '...'}</div>
-      :
-      <div>{markdown(pull.body)}</div>
-      } <br />
-      <div>
-        <a href={pull.pull_request.html_url} className="event-link">View on Github</a>
-      </div>
-    </span>
-  </ListGroupItem>
+    <ListGroupItem key={pull.id} className="pull-event">
+      <span className="content">
+        <span className="content-user">@{pull.user.login} opened a pull request.</span>{'   '}
+        <i className="content-updated">{moment(pull.updated_at).startOf('hour').fromNow()}.</i><br />
+        <div className="repo-name">{pull.html_url.split('/')[4]} / {pull.html_url.split('/')[3]}</div><br />
+        <div>*  {pull.title}  *</div><br />
+        {/* <div><b>Ranking:</b> {pull.ranking}</div> */}
+        {markdown(pull.body).length > 500
+        ?
+        <div>{markdown(pull.body).slice(0, 500) + '...'}</div>
+        :
+        <div>{markdown(pull.body)}</div>
+        } <br />
+        <div>
+          <a href={pull.pull_request.html_url} className="event-link">View on Github</a>
+        </div>
+      </span>
+    </ListGroupItem>
+  </div>
 );
 
 export default PullRequest;
