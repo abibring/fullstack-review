fab7c73ffae011429a451e45df3f5501263b320c
Alon Bibring <alon.bibring@gmail.com>
2018-12-24 22:44:19 -0500

    removed navbar import of deleted pic

--- client/src/components/NavigationBar.jsx
+++ client/src/components/NavigationBar.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Navbar, NavItem, Nav, Button } from 'react-bootstrap';
-import logo from '../../dist/img/new_logo.png';
 
 export default class NavigationBar extends Component {
   constructor(props) {
