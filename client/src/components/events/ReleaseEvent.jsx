a01e986b74d2a46712073d8de462ba234ab1ab94
Alon Bibring <alon.bibring@gmail.com>
2018-12-25 12:56:41 -0500

    converted release into class component so I can check if user wants to be rerouted

--- client/src/components/events/ReleaseEvent.jsx
+++ client/src/components/events/ReleaseEvent.jsx
@@ -1,32 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
 import moment from 'moment';
 import markdown from 'remove-markdown';
 import { ListGroupItem } from 'react-bootstrap';
 
-const ReleaseEvent = ({ release }) => (
-  <div className="outer-event">
-    <span className="content-img">
-      <img src={release.author.avatar_url} alt='avatar of release author' className="feed-img" />
-    </span>
-    <ListGroupItem className="release-event">
-      <span className="content">
-        <span className="content-user">@{release.author.login} pushed a new release.</span>{'    '}
-        <i className="content-updated">{moment(release.published_at).startOf('hour').fromNow()}.</i><br /><br/>
-        <div className="repo-name">{release.html_url.split('/')[3]} / {release.html_url.split('/')[4]}</div><br />
-        <div className="release-version">{release.tag_name}</div><br />
-        {/* <div><b>Ranking:</b> {release.ranking}</div> */}
-        {markdown(release.body).length > 500
-        ?
-          <div>{markdown(release.body).slice(0, 500) + '...'}</div>
-        :
-          <div>{markdown(release.body)}</div>
-        } <br />
-        <div>
-          <a href={release.html_url} className="event-link">View on Github</a>
-        </div>
-      </span>
-    </ListGroupItem>
-  </div>
-);
+// const ReleaseEvent = ({ release }) => (
+//   <div className="outer-event">
+//     <span className="content-img">
+//       <img src={release.author.avatar_url} alt='avatar of release author' className="feed-img" />
+//     </span>
+//     <ListGroupItem className="release-event">
+//       <span className="content">
+//         <span className="content-user">@{release.author.login} pushed a new release.</span>{'    '}
+//         <i className="content-updated">{moment(release.published_at).startOf('hour').fromNow()}.</i><br /><br/>
+//         <div className="repo-name">{release.html_url.split('/')[3]} / {release.html_url.split('/')[4]}</div><br />
+//         <div className="release-version">{release.tag_name}</div><br />
+//         {/* <div><b>Ranking:</b> {release.ranking}</div> */}
+//         {markdown(release.body).length > 500
+//         ?
+//           <div>{markdown(release.body).slice(0, 500) + '...'}</div>
+//         :
+//           <div>{markdown(release.body)}</div>
+//         } <br />
+//         <div>
+//           <a onClick={() => } href={release.html_url} className="event-link">View on Github</a>
+//         </div>
+//       </span>
+//     </ListGroupItem>
+//   </div>
+// );
+
+export default class ReleaseEvent extends Component {
+  constructor(props) {
+    super(props);
+    this.confirmRedirect = this.confirmRedirect.bind(this);
+  }
+  confirmRedirect() {
+    window.onbeforeunload = () => {
+      return 'You will be leaving githubfeed.com. Is that what you would like?'
+    }
+  }
+
+  render() {
+    return (
+      <div className="outer-event">
+        <span className="content-img">
+          <img src={release.author.avatar_url} alt='avatar of release author' className="feed-img" />
+        </span>
+        <ListGroupItem className="release-event">
+          <span className="content">
+            <span className="content-user">@{release.author.login} pushed a new release.</span>{'    '}
+            <i className="content-updated">{moment(release.published_at).startOf('hour').fromNow()}.</i><br /><br/>
+            <div className="repo-name">{release.html_url.split('/')[3]} / {release.html_url.split('/')[4]}</div><br />
+            <div className="release-version">{release.tag_name}</div><br />
+            {/* <div><b>Ranking:</b> {release.ranking}</div> */}
+            {markdown(release.body).length > 500
+            ?
+              <div>{markdown(release.body).slice(0, 500) + '...'}</div>
+            :
+              <div>{markdown(release.body)}</div>
+            } <br />
+            <div>
+              <a onClick={() => } href={release.html_url} className="event-link">View on Github</a>
+            </div>
+          </span>
+        </ListGroupItem>
+      </div>
+    );
+  }
+
+}
 
 export default ReleaseEvent;
