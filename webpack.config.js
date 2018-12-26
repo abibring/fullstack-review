c4d990d9ff959c76937b4d9295a0aeaa3402e63f
Alon Bibring <alon.bibring@gmail.com>
2018-12-26 18:56:47 -0500

    adjusting webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,6 @@ const path = require('path');
 const Dotenv = require('dotenv-webpack');
 const SRC_DIR = path.join(__dirname, '/client/src');
 const DIST_DIR = path.join(__dirname, '/client/dist');
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const __PROD__ = process.env.NODE_ENV === 'production'
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
@@ -29,36 +27,16 @@ module.exports = {
             loader: 'url-loader'
           }
         ]
-      },
-      {
-        test: /\.css$/,
-        include: [
-          `${SRC_DIR}/../dist`,
-          /node_modules/
-        ],
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            {
-              loader: 'css-loader',
-              options: {
-                importLoaders: true,
-                minimize: __PROD__
-              }
-            }
-          ]
-        })
       }
     ]
   },
   plugins: [
     new Dotenv({
       path: './.env',
-      safe: false,
+      safe: true,
       systemvars: true,
-      silent: false
-    }),
-    ExtractTextPlugin('style.css')
+      silent: true
+    })
   ],
   node: {
     fs: 'empty'
