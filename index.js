0835a141d07a5cced36f7bf365578a344428808b
Alon Bibring <alon.bibring@gmail.com>
2018-12-26 18:41:22 -0500

    removed compression from express and added webpack compression

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const router = require('./server/routes/index.js');
 const cookieSession = require('cookie-session');
-const compression = require('compression');
 const path = require('path');
 require('dotenv').config();
 const app = express();
@@ -14,18 +13,19 @@ app.use(
     saveUninitialized: true,
     maxAge: 24 * 60 * 60 * 1000 // 24 hours
   })
-);
-
-app.use(express.static(path.join(__dirname, '/client/dist'), { redirect: false }));
+  );
+  
+  app.use(express.static(path.join(__dirname, '/client/dist'), { redirect: false }));
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  
+  app.use('/', router);
+  app.use('/*', (req, res) => res.redirect('/'));
+  let port = process.env.PORT || 3000;
+  
+  app.listen(port, () => console.log(`listening on port ${port}`));
+// const compression = require('compression');
 // app.use(compression({
 //   level: 2,               // set compression level from 1 to 9 (6 by default)
 //   filter: shouldCompress, // set predicate to determine whether to compress
 // }));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-app.use('/', router);
-app.use('/*', (req, res) => res.redirect('/'));
-let port = process.env.PORT || 3000;
-
-app.listen(port, () => console.log(`listening on port ${port}`));
