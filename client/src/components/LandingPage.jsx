1b2efbbade7bd496b0a1495acd01e9f67218f8c2
Alon Bibring <alon.bibring@gmail.com>
2018-12-26 18:24:13 -0500

    changed LandingPage from class to functional component

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -2,25 +2,20 @@ import React, { Component } from 'react';
 import { Jumbotron } from 'react-bootstrap';
 import Login from './Login.jsx';
 
-export default class LandingPage extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <Jumbotron className="jumbotron">
-        <div className="jumbotron-content">
-          <h1>The Useful Github Feed</h1>
-          <p>
-            This is a better version of the Github feed, which allows you to track the latest information
-            about repos you are watching, contributing to, or using.  Get the latest information about any updates,
-            such as pull requests, commits, issues, new repos, and more! Sign in with your Github credentials to get started!
-          </p> <br />
-          <div>
-            <Login />
-          </div>
-        </div>
-      </Jumbotron>
-    );
-  }
-}
+const LandingPage = () => (
+  <Jumbotron className="jumbotron">
+    <div className="jumbotron-content">
+      <h1>The Useful Github Feed</h1>
+      <p>
+        This is a better version of the Github feed, which allows you to track the latest information
+        about repos you are watching, contributing to, or using.  Get the latest information about any updates,
+        such as pull requests, commits, issues, new repos, and more! Sign in with your Github credentials to get started!
+      </p> <br />
+      <div>
+        <Login />
+      </div>
+    </div>
+  </Jumbotron>
+);
+
+export default LandingPage;
