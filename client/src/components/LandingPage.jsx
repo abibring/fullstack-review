ef86308d68049b0ec17fdfbc758b2e52dc709c15
Alon Bibring <alon.bibring@gmail.com>
2018-12-30 17:47:16 -0500

    changing color of header in landingpage

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -5,11 +5,11 @@ import Login from './Login.jsx';
 const LandingPage = () => (
   <Jumbotron className="jumbotron">
     <div className="jumbotron-content">
-      <h1>The Useful Github Feed</h1>
+      <h1 style={{ color: '#39927A' }}>The Useful Github Feed</h1>
       <p>
         This is a useful feed for Github.  Find out the latest updates about repos you
-        are watching, have starred, or are affiliated with.  Get the latest information about your repos, such as
-        pull requests, issues, new releases, and more! Sign in with your Github credentials to get started.
+        are watching, have starred, or are affiliated with.  Get the latest information about your repos, 
+        such as pull requests, issues, new releases, and more! Sign in with your Github credentials to get started.
       </p> <br />
       <div>
         <Login />
