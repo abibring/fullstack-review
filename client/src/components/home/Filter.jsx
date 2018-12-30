4c5acf152e7d04531fe235d11f11085d6175205f
Alon Bibring <alon.bibring@gmail.com>
2018-12-29 20:32:30 -0500

    adding filter

--- client/src/components/home/Filter.jsx
+++ client/src/components/home/Filter.jsx
@@ -5,6 +5,10 @@ export default class Filter extends Component {
   constructor(props) {
     super(props);
     this.state = { repos: [] };
+    this.filterReposByName = this.filterReposByName.bind(this);
+  }
+  componentDidMount() {
+    this.filterReposByName()
   }
   filterReposByName() {
     const { repos } = this.props;
@@ -19,6 +23,8 @@ export default class Filter extends Component {
     const { repos } = this.state;
     return (
       <div>
+        {console.log('PROPS', this.props.repos)}
+        {console.log('STATE', repos)}
         <ButtonToolbar>
           <DropdownButton title="Default button" id="dropdown-size-medium">
             {repos.map((repo, i) => (
