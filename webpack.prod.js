f6ca28347ff5fb05b41aa8dd555e9a00f013327c
Alon Bibring <alon.bibring@gmail.com>
2018-12-31 12:45:33 -0500

    editing type in html

---
+++ webpack.prod.js
@@ -0,0 +1,44 @@
+const Dotenv = require('dotenv-webpack');
+const path = require('path');
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.(pdf|jpg|png|gif|svg|ico)$/,
+        use: [
+          {
+            loader: 'file-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new Dotenv({
+      path: './.env',
+      safe: true,
+      systemvars: true,
+      silent: true
+    })
+  ],
+  node: {
+    fs: 'empty'
+  }
+};
