5fcb2c7205c15ee9a86cd4f2bf7af323a47bcfec
Alon Bibring <alon.bibring@gmail.com>
2018-12-30 19:03:29 -0500

    adding new logo

--- index.js
+++ index.js
@@ -23,8 +23,3 @@ app.use('/*', (req, res) => res.redirect('/'));
 let port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`listening on port ${port}`));
-// const compression = require('compression');
-// app.use(compression({
-//   level: 2,               // set compression level from 1 to 9 (6 by default)
-//   filter: shouldCompress, // set predicate to determine whether to compress
-// }));
