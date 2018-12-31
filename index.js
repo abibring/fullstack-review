5f44c8bdaad63a6b58ea0f9c3f8ffbb2c69fb203
Alon Bibring <alon.bibring@gmail.com>
2018-12-31 18:41:53 -0500

    changing html

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
   res.set('Cache-Control', 'no-cache');
   next();
 }); 
-app.use(express.static(path.join(__dirname, '/client/dist'), { redirect: false, maxAge: 86400 }));
+app.use(express.static(path.join(__dirname, '/client/dist'), { redirect: false, maxAge: '86400' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
