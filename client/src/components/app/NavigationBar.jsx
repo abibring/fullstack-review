337ee400257ad2d1ebb9a35bfc26f44c30632192
Alon Bibring <alon.bibring@gmail.com>
2018-12-30 19:37:53 -0500

    adding styling to logo pic

--- client/src/components/app/NavigationBar.jsx
+++ client/src/components/app/NavigationBar.jsx
@@ -8,30 +8,32 @@ const NavigationBar = ({ signOut }) => {
   const whiteFont = { color: 'white' };
   return (
     <Navbar className="navbar">
-      <Image src={logo} alt="logo" />
-    <Nav>
-      <NavItem eventKey={1} href="https://www.linkedin.com/in/alon-bibring-45117458/" style={{ opacity: .7 }}>
-        <div style={whiteFont}>Made with &#9825; </div>
-        <div style={whiteFont}>
-          by <i style={{ textDecoration: 'underline' }}>Alon Bibring</i> in NYC.
-        </div>
-      </NavItem>
-    </Nav>
-    <Nav>
-      <NavItem >
-        <div style={{ color: 'white', fontSize: 14 }}>GithubFeed</div>
-        <div style={{ color: 'white', fontSize: 12, opacity: .7 }}>Github updates you care about.</div>
-      </NavItem>
-    </Nav>
-    <Nav pullRight>
-      <NavItem eventKey={2} href="#" className="nav-user-info">
-        <div className="nav-username">@{username}</div>
-        <button className="nav-signout" onClick={signOut}>{'    '}Sign Out</button>
-      </NavItem>
-      <NavItem>
-        <Image src={userAvatar} className="user-pic"/>
-      </NavItem>
-    </Nav>
+      {/* <Navbar.Brand> */}
+        <Image src={logo} alt="logo" className="user-pic" />
+      {/* </Navbar.Brand> */}
+      <Nav>
+        <NavItem eventKey={1} href="https://www.linkedin.com/in/alon-bibring-45117458/" style={{ opacity: .7 }}>
+          <div style={whiteFont}>Made with &#9825; </div>
+          <div style={whiteFont}>
+            by <i style={{ textDecoration: 'underline' }}>Alon Bibring</i> in NYC.
+          </div>
+        </NavItem>
+      </Nav>
+      <Nav>
+        <NavItem >
+          <div style={{ color: 'white', fontSize: 14 }}>GithubFeed</div>
+          <div style={{ color: 'white', fontSize: 12, opacity: .7 }}>Github updates you care about.</div>
+        </NavItem>
+      </Nav>
+      <Nav pullRight>
+        <NavItem eventKey={2} href="#" className="nav-user-info">
+          <div className="nav-username">@{username}</div>
+          <button className="nav-signout" onClick={signOut}>{'    '}Sign Out</button>
+        </NavItem>
+        <NavItem>
+          <Image src={userAvatar} className="user-pic"/>
+        </NavItem>
+      </Nav>
   </Navbar>
   );
 }
