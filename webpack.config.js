ce3a2154558b3bf7c402f725185d3d4adebdbe9e
Alon Bibring <alon.bibring@gmail.com>
2018-12-31 11:08:20 -0500

    editing landing page

--- webpack.config.js
+++ webpack.config.js
@@ -29,8 +29,11 @@ module.exports = {
           {
             loader: 'image-webpack-loader',
             options: {
-              bypassOnDebug: true, // webpack@1.x
-              disable: true, // webpack@2.x and newer
+              disable: true,
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
             },
           },
         ],
