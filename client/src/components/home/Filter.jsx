663401aa42b0e7e83fcdbb4c6e6af34c65c25d06
Alon Bibring <alon.bibring@gmail.com>
2018-12-31 17:05:20 -0500

    descontructed prop names

--- client/src/components/home/Filter.jsx
+++ client/src/components/home/Filter.jsx
@@ -9,12 +9,12 @@ export default class Filter extends Component {
 
   render() {
     const { name } = this.state;
-    const { onSelect } = this.props;
+    const { onSelect, names } = this.props;
     return (
       <div>
         <ButtonToolbar>
           <DropdownButton title={name} id="dropdown-size-medium">
-            {repoNames.map((repo, i) => (
+            {names.map((repo, i) => (
               <MenuItem eventKey={repo} key={i} onSelect={(e) => {
                 onSelect(e)
                 this.setState({ name: e })
