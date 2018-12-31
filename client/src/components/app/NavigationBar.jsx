32ec4dee8327f6f148fd602fd1c2a6040fd78875
Alon Bibring <alon.bibring@gmail.com>
2018-12-30 19:56:32 -0500

    moving logo aorund

--- client/src/components/app/NavigationBar.jsx
+++ client/src/components/app/NavigationBar.jsx
@@ -9,10 +9,10 @@ const NavigationBar = ({ signOut }) => {
   return (
     <Navbar className="navbar">
       <Nav>
-        <NavItem key={1} href="#">
-          <Image src={logo} alt="logo" className="logo" />
-        </NavItem>
+        {/* <NavItem key={1} href="#">
+        </NavItem> */}
         <NavItem eventKey={2} href="https://www.linkedin.com/in/alon-bibring-45117458/" style={{ opacity: .7 }}>
+          <Image src={logo} alt="logo" className="logo" />
           <div style={whiteFont}>Made with &#9825; </div>
           <div style={whiteFont}>
             by <i style={{ textDecoration: 'underline' }}>Alon Bibring</i> in NYC.
