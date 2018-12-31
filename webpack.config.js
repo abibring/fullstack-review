3107c08a269072a6a5b21480b379c70fefb80d78
Alon Bibring <alon.bibring@gmail.com>
2018-12-31 03:01:46 -0500

    fixing styling

--- webpack.config.js
+++ webpack.config.js
@@ -30,11 +30,6 @@ module.exports = {
           }
         ]
       },
-      {
-        test: /\.css$/,
-        include: /node_modules/,
-        use: ['style-loader', 'css-loader']
-      }
     ]
   },
   plugins: [
