f52d4c1fb00de7900738aef99e018c660609edc5
Alon Bibring <alon.bibring@gmail.com>
2019-01-06 10:56:29 -0500

    adjusting clientId in login to take from process.env or use string provided for clientId - using process.env for local deployment and string for prod

--- client/src/components/app/Login.jsx
+++ client/src/components/app/Login.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 
-const Login = () => (
-  <Button
-    href={`https://github.com/login/oauth/authorize?client_id=fd744291ef509a6a54cc&scope=user%20repo%20notifications`}
-    bsStyle="primary"
-    className="login-button"
-  >
-    Sign In With Github
-  </Button>
-)
+const Login = () => {
+  let clientId = process.env.GITHUB_CLIENT_ID || 'fd744291ef509a6a54cc';
+  return (
+    <Button
+      href={`https://github.com/login/oauth/authorize?client_id=${clientId}&scope=user%20repo%20notifications`}
+      bsStyle="primary"
+      className="login-button"
+    >
+      Sign In With Github
+    </Button>
+  );
+}
 
 export default Login;
 
