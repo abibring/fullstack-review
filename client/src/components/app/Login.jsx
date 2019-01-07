cc5b5797eb9787c51db9704f7b7c3cf351be6c01
Alon Bibring <alon.bibring@gmail.com>
2019-01-07 13:52:36 -0500

    adjusting credentials for github

--- client/src/components/app/Login.jsx
+++ client/src/components/app/Login.jsx
@@ -3,13 +3,14 @@ import { Button } from 'react-bootstrap';
 
 
 const Login = () => {
-  let clientId = process.env.GITHUB_CLIENT_ID || 'c1b01256656b3ebf6d23';
+  let clientId = process.env.GITHUB_CLIENT_ID || 'fd744291ef509a6a54cc';
   return (
     <Button
       href={`https://github.com/login/oauth/authorize?client_id=${clientId}&scope=user%20repo%20notifications`}
       bsStyle="primary"
       className="login-button"
     >
+    {console.log('CLIENT ID', clientId)}
       Sign In With Github
     </Button>
   );
